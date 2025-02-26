d7e82f96a6fe43c558174e5a979b79af4f9c8352
oyk0510 <youkyungoh0510@naver.com>
2025-02-26 17:09:33 +0900

    4. control flow(looping and branching)

--- src/main/java/chap04_control_flow/looping_and_branching/level02/normal/Application3.java
+++ src/main/java/chap04_control_flow/looping_and_branching/level02/normal/Application3.java
@@ -1,9 +1,20 @@
-package com.oyk.section02.looping_and_branching.level02.normal;
+package chap04_control_flow.looping_and_branching.level02.normal;
 
 import java.util.Scanner;
 
 public class Application3 {
     public static void main(String[] args) {
+
+        /* 정수를 입력받아 1부터 입력받은 정수까지
+         * 홀수이면 "수", 짝수이면 "박"이 정수만큼 누적되어 출력되게 작성하시오.
+         *
+         * -- 입력 예시 --
+         * 정수를 입력하세요 : 5
+         *
+         * -- 출력 예시 --
+         * 수박수박수
+         * */
+
         Scanner sc = new Scanner(System.in);
         System.out.println("정수를 입력하세요 : ");
         int num = sc.nextInt();
